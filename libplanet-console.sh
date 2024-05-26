10c295a50ce5ad7c8789ff848dcc62c6a85d3ef8
s2quake <han0210@netsgo.com>
2024-05-26 22:20:25 +0900

    chore: Fix an error message when not building.

--- libplanet-console.sh
+++ libplanet-console.sh
@@ -4,7 +4,8 @@ workspace=$(realpath $(dirname "$0"))
 filename=$workspace/.bin/libplanet-console/libplanet-console.dll
 
 if [ ! -f $filename ]; then
-  echo -e "'$filename' not found.\nPlease run 'dotnet build' first."
+  echo "'$filename' not found."
+  echo "Please run 'dotnet build' first."
   exit 1
 fi
 
