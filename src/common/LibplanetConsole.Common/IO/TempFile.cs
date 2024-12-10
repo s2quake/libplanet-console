ae30e1856bd17f026e7a4a11b7d3c3812abeff7d
s2quake <jeesu@planetariumhq.com>
2024-12-10 10:24:44 +0900

    fix: Change genesis arg name to genesis-path

--- src/common/LibplanetConsole.Common/IO/TempFile.cs
+++ src/common/LibplanetConsole.Common/IO/TempFile.cs
@@ -2,35 +2,37 @@ namespace LibplanetConsole.Common.IO;
 
 public sealed class TempFile : IDisposable
 {
+    private static readonly string _directory = Path.Combine(
+        Path.GetTempPath(),
+        AppDomain.CurrentDomain.FriendlyName,
+        $"{Environment.ProcessId}");
+
+    static TempFile()
+    {
+        DirectoryUtility.EnsureDirectory(_directory);
+        AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+
+        static void CurrentDomain_ProcessExit(object? sender, EventArgs e)
+            => DirectoryUtility.TryDelete(_directory, recursive: true);
+    }
+
     public TempFile()
     {
-        FileName = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
     }
 
-    public string FileName { get; }
+    private TempFile(string content) => File.WriteAllText(FileName, content);
 
-    public static implicit operator string(TempFile tempFile)
-    {
-        return tempFile.FileName;
-    }
+    private TempFile(byte[] bytes) => File.WriteAllBytes(FileName, bytes);
 
-    public static TempFile WriteAllText(string content)
-    {
-        var tempFile = new TempFile();
-        File.WriteAllText(tempFile.FileName, content);
-        return tempFile;
-    }
+    public string FileName { get; } = Path.Combine(_directory, Path.GetRandomFileName());
 
-    public void Dispose()
-    {
-        if (File.Exists(FileName) == true)
-        {
-            File.Delete(FileName);
-        }
-    }
+    public static implicit operator string(TempFile tempFile) => tempFile.FileName;
 
-    public override string ToString()
-    {
-        return FileName;
-    }
+    public static TempFile WriteAllText(string content) => new(content);
+
+    public static TempFile WriteAllBytes(byte[] bytes) => new(bytes);
+
+    public void Dispose() => FileUtility.TryDelete(FileName);
+
+    public override string ToString() => FileName;
 }
