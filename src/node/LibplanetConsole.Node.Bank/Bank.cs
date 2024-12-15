063a0650ba67e68d4c600066ffd0faea2165d008
s2quake <han0210@netsgo.com>
2024-12-15 22:13:21 +0900

    fix: Fix an issue where the currency collection fails to collect currencies

--- src/node/LibplanetConsole.Node.Bank/Bank.cs
+++ src/node/LibplanetConsole.Node.Bank/Bank.cs
@@ -1,14 +1,9 @@
 using Libplanet.Action.State;
 using LibplanetConsole.Bank.Actions;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace LibplanetConsole.Node.Bank;
 
-internal sealed class Bank(
-    IServiceProvider serviceProvider,
-    INode node,
-    IBlockChain blockChain,
-    CurrencyCollection currencies)
+internal sealed class Bank(INode node, IBlockChain blockChain)
     : NodeContentBase(nameof(Bank)), IBank
 {
     public async Task TransferAsync(
@@ -31,23 +26,5 @@ internal sealed class Bank(
             => blockChain.GetWorldState().GetBalance(address, currency);
 
         return await Task.Run(Action, cancellationToken);
-    }
-
-    protected override async Task OnStartAsync(CancellationToken cancellationToken)
-    {
-        var currencyProviders = serviceProvider.GetServices<ICurrencyProvider>();
-        var items = currencyProviders.SelectMany(provider => provider.Currencies);
-        foreach (var item in items)
-        {
-            currencies.Add(item.Code, item.Currency);
-        }
-
-        await Task.CompletedTask;
-    }
-
-    protected override async Task OnStopAsync(CancellationToken cancellationToken)
-    {
-        currencies.Clear();
-        await Task.CompletedTask;
     }
 }
--- src/node/LibplanetConsole.Node.Bank/ServiceCollectionExtensions.cs
+++ src/node/LibplanetConsole.Node.Bank/ServiceCollectionExtensions.cs
@@ -13,7 +13,8 @@ public static class ServiceCollectionExtensions
             .AddSingleton<IBank>(s => s.GetRequiredService<Bank>());
 
         @this.AddSingleton<CurrencyCollection>()
-            .AddSingleton<ICurrencyCollection>(s => s.GetRequiredService<CurrencyCollection>());
+            .AddSingleton<ICurrencyCollection>(s => s.GetRequiredService<CurrencyCollection>())
+            .AddSingleton<INodeContent>(s => s.GetRequiredService<CurrencyCollection>());
 
         @this.AddSingleton<ICommand, BankCommand>();
         return @this;
